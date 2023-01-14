fd22468651322e42946bce259bdccbd05ac01f22
mehdi <mehdisamiyann@gmail.com>
2023-01-14 17:53:43 +0330

    complete 19

--- project-pdfDownloader/src/App.js
+++ project-pdfDownloader/src/App.js
@@ -1,26 +1,73 @@
+import axios from 'axios';
 import React from 'react';
-import { Icon } from 'react-icons-kit'
-import {eye} from "react-icons-kit/feather/eye";
-import {eyeOff} from "react-icons-kit/feather/eyeOff";
 import { useState } from 'react';
+import jsPDF from 'jspdf'
+import autoTable from 'jspdf-autotable'
+import Fade from 'react-reveal/Fade';
+import Jump from 'react-reveal/Jump';
 
 function App() {
-  const [type ,settype] = useState('password');
-  const [icon ,seticon] = useState(eyeOff);
-
-  const handleClick = () => {
-    settype( type === 'password' ? 'text' : 'password')
-    seticon( icon === eyeOff ? eye : eyeOff)
+  const [data, setData] = useState([]);
+  const getData = () => {
+    axios.get("https://jsonplaceholder.typicode.com/todos")
+      .then((resp) => {
+        setData(resp.data)
+        console.log(data)
+      })
   }
-  
+  const downloadPdf = () => {
+    const pdf = new jsPDF();
+    pdf.autoTable({ html: '#table' })
+    pdf.save("user.pdf")
+
+  }
+
   return (
-   <div className='wrapper'>
-    <div className='input-fileds'>
-      <span onClick={handleClick}><Icon icon={icon} size={25}></Icon></span>
-      <input type={type}></input>
-      
-    </div>
-   </div>
+    <div className='container'>
+
+
+      <h1 className='text-center py-5'>دانلود جدول به‌صورت pdf</h1>
+      <div className='pb-5'>
+        <button className='btn btn-primary  ' onClick={getData}>نمایش جدول</button>
+      </div>
+      {
+        data.length > 0 && (
+
+          <div className='download-data'>
+            <Jump>
+              <button onClick={downloadPdf} className='btn btn-success'>دانلود جدول</button>
+
+              <h2 className='py-5 text-center'>جدول اطلاعات</h2>
+            </Jump>
+            <Fade bottom>
+              <table className='table table-borderless' id='table'>
+                <thead>
+                  <tr>
+                    <th>ID</th>
+                    <th>Title</th>
+                    <th>Status</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {
+                    data.map((item) => {
+                      return (
+
+                        <tr key={item.id}>
+                          <td>{item.id}</td>
+                          <td>{item.title}</td>
+                          <td>{String(item.completed)}</td>
+                        </tr>
+                      )
+                    })
+                  }
+                </tbody>
+              </table>
+            </Fade>
+          </div>
+        )
+      }
+    </div >
   );
 }
 
