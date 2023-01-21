e354e3184ce659f5bb037c88716b861036751715
mehdi <mehdisamiyann@gmail.com>
2023-01-22 03:11:09 +0330

    comp project 22

--- project-searchBar/front/src/App.js
+++ project-searchBar/front/src/App.js
@@ -1,16 +1,28 @@
 import React from 'react';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { Users } from './users'
 import Table from './Table';
+import axios from 'axios';
 function App() {
 
   const [query, setQuery] = useState('');
-  const keys = ['first_name','last_name','email']
-  const search = (data) => {
-    return data.filter((item) =>
-    keys.some((key)=> item[key].toLowerCase().includes(query))
-    );
-  }
+  const [data, setData] = useState([]);
+
+  // const keys = ['first_name', 'last_name', 'email'];
+
+  // const search = (data) => {
+  //   return data.filter((item) =>
+  //   keys.some((key)=> item[key].toLowerCase().includes(query))
+  //   );
+  // }
+
+  useEffect(() => {
+    const fetchSearch = async () => {
+      const res = await axios.get(`https://localhost:5000?q=${query}`)
+      setData(res.data);
+    }
+    fetchSearch()
+  }, [query]);
 
   console.log(query)
 
@@ -19,7 +31,7 @@ function App() {
     <div className='app'>
       <input placeholder='جستحو' type='text' onChange={(e) => setQuery(e.target.value)} ></input>
 
-      <Table data={search(Users)} />
+      <Table data={data} />
 
     </div>
   );
